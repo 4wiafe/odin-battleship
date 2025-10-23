a26cccec26263fa139011f4ea6561b8570a4680c
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-10-23 15:05:16 +0000

    Add test to verify ship initializes with zero hits

--- src/ships/ships.test.js
+++ src/ships/ships.test.js
@@ -5,4 +5,8 @@ describe('ship properties', () => {
   test('length of ship', () => {
     expect(ship1.length).toEqual(5);
   });
+
+  test('ship damage taken', () => {
+    expect(ship1.timesHit).toEqual(0);
+  });
 });
