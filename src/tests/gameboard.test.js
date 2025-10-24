6198bc7a3b1d8b6dbd5d75d8dbb7a79d1c8fb910
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-10-24 03:59:57 +0000

    Verify Gameboard has 10 rows

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,4 +6,8 @@ describe('gameboard behavior', () => {
   test(`should create a new gameboard instance successfully`, () => {
     expect(gameboard).toBeTruthy();
   });
+
+  test('board should be an array', () => {
+    expect(gameboard.board.length).toBe(10);
+  });
 });
