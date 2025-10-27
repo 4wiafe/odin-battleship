6a3ba4269779b6bdff95792dfe372a8d731d46c2
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-10-27 11:20:41 +0000

    Add boundary check for ship placement on gameboard

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,25 +1,29 @@
 import { Ships } from "../ships/ships.js";
 
-const ship = new Ships(3);
+const ship1 = new Ships(3);
+const ship2 = new Ships(4);
 
 class Gameboard {
   board = Array.from({ length: 10 }, () => Array(10).fill(null));
 
   placeShip(ship, x, y, direction) {
+    if (
+      (direction === 'horizontal' && x + ship.length > this.board.length) ||
+      (direction === 'vertical' && y + ship.length > this.board.length)
+    ) return false;
+
     for (let i = 0; i < ship.length; i++) {
       if (direction === 'horizontal') {
-        this.board[y][x + i] = ship;
-      } else if (direction === 'vertical') {
-        this.board[y + i][x] = ship;
+        
       }
     }
 
-    console.log(this.board);
     return true;
   }
 }
 
 const newBoard = new Gameboard();
-console.log(newBoard.placeShip(ship, 0, 0, 'horizontal'));
+console.log(newBoard.placeShip(ship1, 0, 0, 'horizontal'));
+console.log(newBoard.placeShip(ship2, 0, 0, 'horizontal'));
 
 export { Gameboard };
