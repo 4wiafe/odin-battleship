bdfe7a65ebf9ed9087d310ec761efd76b22a23d5
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-10-29 09:33:16 +0000

    Add player.js module and player.test.js file

--- src/tests/ships.test.js
+++ src/tests/ships.test.js
@@ -1,20 +1,20 @@
 import { Ships } from "../ships/ships";
 const ship1 = new Ships(5);
 
-describe('ship properties', () => {
-  test('length of ship', () => {
+describe("ship properties", () => {
+  test("length of ship", () => {
     expect(ship1.length).toEqual(5);
   });
 
-  test('ship damage taken', () => {
+  test("ship damage taken", () => {
     expect(ship1.timesHit).toEqual(0);
   });
 
-  test('check damage taken', () => {
+  test("check damage taken", () => {
     expect(ship1.hit()).toBe(1);
   });
 
-  test('has ship sunk', () => {
+  test("has ship sunk", () => {
     expect(ship1.isSunk()).toBe(false);
   });
 });
