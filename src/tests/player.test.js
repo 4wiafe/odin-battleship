22e1d62ea87b47e4a91a02bd503eaaaefed0aeb0
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-02 15:30:30 +0000

    Remove gameboard and ship instances

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,15 +1,9 @@
 import { Player } from "../player/player";
-import { Gameboard } from "../gameboard/gameboard";
-import { Ships } from "../ships/ships";
 
 let player;
-let gameboard;
-let ship;
 
 beforeEach(() => {
   player = new Player();
-  gameboard = new Gameboard();
-  ship = new Ships(5);
 });
 
 describe("player behavior", () => {
