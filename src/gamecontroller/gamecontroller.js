d590fbe81d28f03c02bf6dc600557ee05fa09a44
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-07 11:11:00 +0000

    Add executeTurn logic for player and computer
    Added the executeTurn(x, y) method to manage both player and computer
    attack sequences.

--- src/gamecontroller/gamecontroller.js
+++ src/gamecontroller/gamecontroller.js
@@ -22,6 +22,67 @@ class GameController {
     return (this.currentTurn =
       this.currentTurn === this.player ? this.computer : this.player);
   }
+
+  executeTurn(x, y) {
+    const currentTurn = this.currentTurn;
+    const player = this.player;
+    const computer = this.computer;
+    let target;
+    let attackResult;
+
+    if (currentTurn === player) {
+      target = computer.gameboard;
+      attackResult = player.makeMove(target, x, y);
+
+      if (attackResult === "already hit") {
+        console.log("Spot already hit. Try again with new coordinates.");
+
+        return "already hit";
+      } else if (attackResult === "miss") {
+        this.switchTurn();
+
+        return "miss";
+      } else if (attackResult === "hit") {
+        if (target.allShipsSunk()) {
+          this.isGameOver = true;
+          this.winner = currentTurn;
+
+          return "Game over!"
+        } else {
+          console.log("Hit! Attack again with new coordinates.");
+        }
+      }
+    } else {
+      target = player.gameboard;
+      attackResult = computer.makeMove(target, x, y);
+
+      if (attackResult === "miss") {
+        this.switchTurn();
+
+        return "miss";
+      } else if (attackResult === "already hit") {
+        console.log("Chosen spot already hit. Generating new attack coordinates...");
+        const newX = Math.floor(Math.random() * 10);
+        const newY = Math.floor(Math.random() * 10);
+
+        attackResult = computer.makeMove(target, newX, newY);
+      } else if (attackResult === "hit") {
+        if (target.allShipsSunk()) {
+          this.isGameOver = true;
+          this.winner = currentTurn;
+
+          return "Game over!";
+        } else {
+          const newX = Math.floor(Math.random() * 10);
+          const newY = Math.floor(Math.random() * 10);
+
+          attackResult = computer.makeMove(target, newX, newY);
+        }
+      }
+    }
+
+    return attackResult;
+  }
 }
 
 export { GameController };
