2b1293911d1495a8ad17754f50a21a4f377b55ed
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-10 21:40:15 +0000

    Add test to verify that data is saved to storage
    Data saved to localStorage should return true upon success

--- src/tests/storageservice.test.js
+++ src/tests/storageservice.test.js
@@ -13,4 +13,17 @@ describe("save, load and clear data", () => {
   test("should have a storageService instance", () => {
     expect(storageService).toBeTruthy();
   });
+
+  
+  test("should have a save method", () => {
+    gameController.initialize();
+
+    jest.spyOn(Storage.prototype, "setItem");
+    const result = storageService.save("gameData", gameController);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "gameData",
+      JSON.stringify(gameController));
+    expect(result).toBe(true);
+  });
 });
