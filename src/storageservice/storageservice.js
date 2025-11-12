ccd3d506b1e54efb4431518772f33a08666202ed
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-12 14:50:52 +0000

    Fixed bugs in test and added a save() method

--- src/storageservice/storageservice.js
+++ src/storageservice/storageservice.js
@@ -1,7 +1,18 @@
 import { GameController } from "../gamecontroller/gamecontroller.js";
 
+const KEY = "battleshipGameData";
+
 class StorageService {
-  
+  static save(gamecontroller) {
+    try {
+      const gameData = gamecontroller.toJSON();
+      localStorage.setItem(KEY, JSON.stringify(gameData));
+      return true;
+    } catch (error) {
+      console.error("Failed to save game data", error);
+      return false;
+    }
+  }
  }
 
 export { StorageService };
