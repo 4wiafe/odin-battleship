e573ce720945063bb14e3944f09e964abbb84543
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-14 22:07:56 +0000

    Add AppController class

---
+++ src/appcontroller/appcontroller.js
@@ -0,0 +1,10 @@
+import { GameController } from "../gamecontroller/gamecontroller.js";
+import { StorageService } from "../storageservice/storageservice.js";
+
+class AppController{ 
+  constructor() {
+    this.gameController = null;
+  }
+}
+
+export { AppController };
