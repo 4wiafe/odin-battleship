87f3634acd7a51d112eb4b11eb19b388623cfde6
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-17 15:36:17 +0000

    Refactor tests for initialize() method

--- src/appcontroller/appcontroller.js
+++ src/appcontroller/appcontroller.js
@@ -2,9 +2,7 @@ import { GameController } from "../gamecontroller/gamecontroller.js";
 import { StorageService } from "../storageservice/storageservice.js";
 
 class AppController{ 
-  constructor() {
-    this.gameController = null;
-  }
+  
 }
 
 export { AppController };
