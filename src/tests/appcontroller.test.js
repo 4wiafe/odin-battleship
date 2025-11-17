a0cf105c94c27f8ff3a1a542dbcc053732ab63cf
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-17 15:36:17 +0000

    Refactor tests for initialize() method

--- src/tests/appcontroller.test.js
+++ src/tests/appcontroller.test.js
@@ -2,32 +2,38 @@ import { AppController } from "../appcontroller/appcontroller.js";
 import { GameController } from "../gamecontroller/gamecontroller.js";
 import { StorageService } from "../storageservice/storageservice.js";
 
+jest.mock("../gamecontroller/gamecontroller.js");
 jest.mock("../storageservice/storageservice.js");
 
 let app;
 
 beforeEach(() => {
   app = new AppController();
-  StorageService.load.mockClear();
-  StorageService.save.mockClear();
-  StorageService.clear.mockClear();
+  jest.clearAllMocks()
 });
 
 describe("AppController", () => {
-  test("appController should exist", () => {
+  test("AppController should exist", () => {
     expect(app).toBeTruthy();
   });
 
-  test("initialize() should load saved game if data exists", () => {
-    const mockSavedGame = { some: "data" };
+  test("initialize() loads saved game if available", () => {
+    const mockGame = { mock: true };
 
-    StorageService.load.mockReturnValue(mockSavedGame);
-    jest.spyOn(GameController, "fromJSON").mockReturnValue({ loaded: true });
-    
+    StorageService.load.mockReturnValue(mockGame);
+
     app.initialize();
 
     expect(StorageService.load).toHaveBeenCalledTimes(1);
-    expect(GameController.fromJSON).toHaveBeenCalledWith(mockSavedGame);
-    expect(app.game).toEqual({ loaded: true });
+    expect(app.game).toBe(mockGame);
+  });
+
+  test("initialize() creates new game if no saved data", () => {
+    StorageService.load.mockReturnValue(false);
+
+    app.initialize();
+
+    expect(GameController).toHaveBeenCalledTimes(1);
+    expect(app.game.initialize).toHaveBeenCalledTimes(1);
   });
 });
