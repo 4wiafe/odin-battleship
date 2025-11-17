5069272e52eb34c468200ac7cea58447d42bb1e2
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-17 16:10:39 +0000

    Add initiliaze() method

--- src/appcontroller/appcontroller.js
+++ src/appcontroller/appcontroller.js
@@ -2,7 +2,21 @@ import { GameController } from "../gamecontroller/gamecontroller.js";
 import { StorageService } from "../storageservice/storageservice.js";
 
 class AppController{ 
-  
+  constructor() {
+    this.game = null;
+  }
+
+  initialize() {
+    const saved = StorageService.load();
+
+    if (!saved) {
+      this.game = new GameController();
+      return this.game.initialize();
+    }
+
+    this.game = saved;
+    return this.game;
+  }
 }
 
 export { AppController };
