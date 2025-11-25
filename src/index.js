fc41de39050f49de1f289864b127c7defcdd83ce
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-25 22:28:10 +0000

    Add cells to player and computer boards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "./styles.css"; 
+import "./styles.css";
+import { UIController } from "./dom/uicontroller.js";
+import { AppController } from "./appcontroller/appcontroller.js";
+
+const app = new AppController();
+const dom = new UIController(app);
+
+dom.renderBoard();
