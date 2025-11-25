95abf091b22cf380b3932b5f3de803270386fb34
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-25 12:57:13 +0000

    Fix bugs in constructor

--- src/dom/uicontroller.js
+++ src/dom/uicontroller.js
@@ -3,13 +3,17 @@ import { AppController } from "../appcontroller/appcontroller.js";
 class UIController{
   constructor(appController) {
     this.appController = appController;
-    
+
     this.displayMessage = document.querySelector(".display-message");
     this.boardContainer = document.querySelector("#board-container");
     this.playerBoard = document.querySelector("#player-board");
     this.computerBoard = document.querySelector("#computer-board");
-    this.randomBtn = this.querySelector(".random-btn");
-    this.randomBtn = this.querySelector(".start-btn");
+    this.randomBtn = document.querySelector(".random-btn");
+    this.randomBtn = document.querySelector(".start-btn");
+  }
+
+  init() {
+    this.appController.initialize();
   }
  }
 
