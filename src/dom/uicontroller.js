a96f508c94e1af0a2f5ba798888ed07fba64e176
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-11-25 12:42:27 +0000

    Add constructor to UIController class

--- src/dom/uicontroller.js
+++ src/dom/uicontroller.js
@@ -1,5 +1,16 @@
 import { AppController } from "../appcontroller/appcontroller.js";
 
-class UIController{ }
+class UIController{
+  constructor(appController) {
+    this.appController = appController;
+    
+    this.displayMessage = document.querySelector(".display-message");
+    this.boardContainer = document.querySelector("#board-container");
+    this.playerBoard = document.querySelector("#player-board");
+    this.computerBoard = document.querySelector("#computer-board");
+    this.randomBtn = this.querySelector(".random-btn");
+    this.randomBtn = this.querySelector(".start-btn");
+  }
+ }
 
 export { UIController };
