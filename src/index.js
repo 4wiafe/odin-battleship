54d987186659f5f41326a8f3e782d35ddf1b060d
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2025-12-26 23:52:13 +0000

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-import { UIController } from "./dom/uicontroller.js";
-import { AppController } from "./appcontroller/appcontroller.js";
-
-const app = new AppController();
-const game = new UIController(app);
-
-game.init();
